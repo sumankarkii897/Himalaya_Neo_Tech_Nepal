bc57fa3372d2b2208cad7cb2b98ccb47a706ccb1
Suman <sumankarkii897@gmail.com>
2026-04-13 18:03:25 +0545

    fix cors production issue

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,15 @@ import helmet from "helmet";
 import {limiter} from "./middleware/security.js";
 dotenv.config();
 const app = express();
+const allowOrigins = [process.env.FRONTEND_URL, "http://localhost:5173","https://himalaya-neo-tech-nepal.vercel.app"];
 app.use(cors(
- {  origin : process.env.FRONTEND_URL,
+ {  origin : allowOrigins,
    credentials : true,
    methods : ["GET","POST","PUT","DELETE"],
    allowedHeaders : ["Content-Type", "Authorization"]}
 
 ))
-
+app.options('*',cors())
 app.use(helmet())
 app.use(limiter)
 
