3c8627453e64566e65bb33e20958860ec9671b8b
Suman <sumankarkii897@gmail.com>
2026-04-13 15:30:16 +0545

    fix:Production database connection setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
+const db = await mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user : process.env.MYSQLUSER,
+    password : process.env.MYSQLPASSWORD,
+    database : process.env.MYSQLDATABASE,
+      port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 
 })
 
