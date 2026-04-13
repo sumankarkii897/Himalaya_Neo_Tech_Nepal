3a1ded6bb552927495b4f88d1c3eb7418a41ee42
Suman <sumankarkii897@gmail.com>
2026-04-13 16:38:13 +0545

    fix:correct mysql pool setup with Database_Url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,16 +2,18 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = await mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user : process.env.MYSQLUSER,
-    password : process.env.MYSQLPASSWORD,
-    database : process.env.MYSQLDATABASE,
-      port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
+// const db = await mysql.createPool({
+//     host: process.env.MYSQLHOST,
+//     user : process.env.MYSQLUSER,
+//     password : process.env.MYSQLPASSWORD,
+//     database : process.env.MYSQLDATABASE,
+//       port: process.env.MYSQLPORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
 
-})
+// })
+const db= mysql.createPool(process.env.DATABASE_URL);
+
 
 console.log("Connected to db successfully");
 
